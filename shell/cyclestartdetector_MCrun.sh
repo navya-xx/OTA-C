f8f17de9f8258cb03ebfb6d3e8ed35b4e3ca2847
navya-xx <navneet.gr8@gmail.com>
2024-05-04 11:16:41 -0400

    sh file update

--- shell/cyclestartdetector_MCrun.sh
+++ shell/cyclestartdetector_MCrun.sh
@@ -30,12 +30,12 @@ do
     do
         cmd_quit="screen -S csdtest_nuc_${nuc_dev} -X quit"
         cmd_main="screen -L -Logfile ${LOGFOLDER}/logfile_${nuc_dev}_${i}.log -S csdtest_nuc_${nuc_dev} -d -m ${LEAF_HOME_NUC}/OTA-C/cpp/build/test_cyclestartdetector serial=${nuc_dev} ${LEAF_DEVICES_NUC_ID[$dev_counter]}"
-        (( dev_counter++ ))
         echo -e "\t Leaf Device ${LEAF_DEVICES_NUC_ID[$dev_counter]}"
         eval "${cmd_quit}"
         eval "${cmd_main}"
         echo -e "\t${cmd_quit}"
         echo -e "\t${cmd_main}\n"
+        (( dev_counter++ ))
     done
 
     echo -e "\n DEVICE NUC01\n"
@@ -44,12 +44,12 @@ do
     do
         cmd_quit="screen -S csdtest_nuc01_${nuc01_dev} -X quit"
         cmd_main="screen -L -Logfile ${LOGFOLDER}/logfile_${nuc01_dev}_${i}.log -S csdtest_nuc01_${nuc01_dev} -d -m ${LEAF_SSH_PREFIX_NUC01} ${LEAF_HOME_NUC01}/OTA-C/cpp/build/test_cyclestartdetector serial=${nuc01_dev} ${LEAF_DEVICES_NUC01_ID[$dev_counter]}"
-        (( dev_counter++ ))
         echo -e "\t Leaf Device ${LEAF_DEVICES_NUC01_ID[$dev_counter]}"
         eval "${cmd_quit}"
         eval "${cmd_main}"
         echo -e "\t${cmd_quit}"
         echo -e "\t${cmd_main}\n"
+        (( dev_counter++ ))
     done
 
     echo -e "\n DEVICE RPI4COMPUTE\n"
@@ -58,24 +58,24 @@ do
     do
         cmd_quit="screen -S csdtest_rpi4compute_${rpi4compute_dev} -X quit"
         cmd_main="screen -L -Logfile ${LOGFOLDER}/logfile_${rpi4compute_dev}_${i}.log -S csdtest_rpi4compute_${rpi4compute_dev} -d -m ${LEAF_SSH_PREFIX_RPI4COMPUTE} ${LEAF_HOME_RPI4COMPUTE}/OTA-C/cpp/build/test_cyclestartdetector serial=${rpi4compute_dev} ${LEAF_DEVICES_RPI4COMPUTE_ID[$dev_counter]}"
-        (( dev_counter++ ))
         echo -e "\t Leaf Device ${LEAF_DEVICES_RPI4COMPUTE_ID[$dev_counter]}"
         eval "${cmd_quit}"
         eval "${cmd_main}"
         echo -e "\t${cmd_quit}"
         echo -e "\t${cmd_main}\n"
+        (( dev_counter++ ))
     done
 
     sleep 2
 
     echo -e "\n DEVICE CENT (NUC01)\n"
     cmd_quit="screen -S cent_nuc01_${CENT_DEVICE_NUC01}_run_${i} -X quit"
-    cmd_main="screen -L -Logfile ${LOGFOLDER}/logfile_${CENT_DEVICE_NUC01}_${i}.log -S cent_nuc01_${CENT_DEVICE_NUC01}_run_${i} -d -m ${LEAF_SSH_PREFIX_NUC01} ${LEAF_HOME_NUC01}/OTA-C/cpp/build/tx_rx_zfc --args serial=${CENT_DEVICE_NUC01} --file /home/nuc01/OTA-C/cpp/storage/mc_run_data.dat --rate 1e6 --bw 2e6 --Tx-N-zfc 79 --Tx-R-zfc 10 --gain 60 --Rx-N-zfc 257"
+    cmd_main="screen -L -Logfile ${LOGFOLDER}/logfile_${CENT_DEVICE_NUC01}_${i}.log -S cent_nuc01_${CENT_DEVICE_NUC01}_run_${i} -d -m ${LEAF_SSH_PREFIX_NUC01} ${LEAF_HOME_NUC01}/OTA-C/cpp/build/tx_rx_zfc --args serial=${CENT_DEVICE_NUC01} --file /home/nuc01/OTA-C/cpp/storage/mc_run_${i}_data.dat --rate 1e6 --bw 2e6 --Tx-N-zfc 79 --Tx-R-zfc 10 --gain 60 --Rx-N-zfc 257"
     echo -e "\t Central Device ${LEAF_DEVICES_RPI4COMPUTE_ID[$dev_counter]}"
     eval "${cmd_quit}"
     eval "${cmd_main}"
-    # echo -e $cmd_quit
-    # echo -e $cmd_main
+    echo -e $cmd_quit
+    echo -e $cmd_main
 
     sleep 10
 done
