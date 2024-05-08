72e6e77d0a5059ae0b428f1a7373b5dcd79ddc9d
navya-xx <navneet.gr8@gmail.com>
2024-05-08 09:49:51 +0200

    asdf

--- cpp/test_cyclestartdetector.cpp
+++ cpp/test_cyclestartdetector.cpp
@@ -67,7 +67,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
             }
         }
 
-        file = currentDir + "/OTA-C/cpp/storage/save_ref_rx_" + device_id;
+        file = currentDir + "/OTA-C/cpp/storage/save_ref_rx_" + device_id + ".dat";
         if (DEBUG)
             std::cout << "Ref signal save to file " << file << std::endl;
     }
@@ -140,7 +140,7 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
         size_t Ref_N_zfc = parser.getValue_int("Ref-N-zfc");
         size_t Ref_m_zfc = parser.getValue_int("Ref-m-zfc");
         size_t Ref_R_zfc = parser.getValue_int("Ref-R-zfc");
-        size_t num_samp_corr = Ref_N_zfc * 1;
+        size_t num_samp_corr = Ref_N_zfc * 2;
         size_t capacity = capacity_mul * std::max(num_samp_corr, max_sample_size);
         float pnr_threshold = parser.getValue_float("pnr-threshold");
         size_t peak_det_tol = parser.getValue_int("peak-det-tol");
