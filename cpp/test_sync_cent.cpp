76ec78738b01f10f157f5497a28a8429261595d2
navya-xx <navneet.gr8@gmail.com>
2024-05-18 00:29:37 +0200

    asdf

--- cpp/test_sync_cent.cpp
+++ cpp/test_sync_cent.cpp
@@ -84,9 +84,10 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
     auto zfc_seq = generateZadoffChuSequence(N_zfc, m_zfc);
     auto ch_zfc_seq = generateZadoffChuSequence(ch_N_zfc, ch_M_zfc);
 
-    size_t ch_seq_reps = 4;
+    size_t ch_seq_reps = 1;
+    size_t ref_ch_gap = ch_N_zfc;
 
-    std::vector<std::complex<float>> buff(N_zfc * R_zfc + 2 * N_zfc + ch_seq_reps * ch_N_zfc, std::complex<float>(0.0, 0.0));
+    std::vector<std::complex<float>> buff(N_zfc * R_zfc + ref_ch_gap + ch_seq_reps * ch_N_zfc, std::complex<float>(0.0, 0.0));
     for (int i = 0; i < N_zfc * R_zfc; ++i)
     {
         buff[i] = zfc_seq[i % N_zfc];
@@ -94,9 +95,9 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
 
     if (ch_seq_reps > 0)
     {
-        for (int i = N_zfc * R_zfc + 2 * N_zfc; i < buff.size(); ++i)
+        for (int i = 0; i < ch_seq_reps * ch_N_zfc; ++i)
         {
-            buff[i] = ch_zfc_seq[(i - (N_zfc * R_zfc + 2 * N_zfc)) % ch_N_zfc];
+            buff[i + N_zfc * R_zfc + ref_ch_gap] = ch_zfc_seq[i % ch_N_zfc];
         }
     }
 
