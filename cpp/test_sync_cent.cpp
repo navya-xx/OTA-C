0d0ae59447336358b244512d6b9c9badb31db22f
navya-xx <navneet.gr8@gmail.com>
2024-05-18 00:34:43 +0200

    asdf

--- cpp/test_sync_cent.cpp
+++ cpp/test_sync_cent.cpp
@@ -84,8 +84,8 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
     auto zfc_seq = generateZadoffChuSequence(N_zfc, m_zfc);
     auto ch_zfc_seq = generateZadoffChuSequence(ch_N_zfc, ch_M_zfc);
 
-    size_t ch_seq_reps = 1;
-    size_t ref_ch_gap = ch_N_zfc;
+    size_t ch_seq_reps = 2;
+    size_t ref_ch_gap = 2 * ch_N_zfc;
 
     std::vector<std::complex<float>> buff(N_zfc * R_zfc + ref_ch_gap + ch_seq_reps * ch_N_zfc, std::complex<float>(0.0, 0.0));
     for (int i = 0; i < N_zfc * R_zfc; ++i)
