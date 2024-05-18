5642605a540766bcd9c42e82f21c3f43e44068d4
navya-xx <navneet.gr8@gmail.com>
2024-05-18 14:56:22 +0200

    asdf

--- shell/node_git_update.sh
+++ shell/node_git_update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-remote_nodes=("rpi4m1@192.168.5.105" "rpi4m2@192.168.5.106" "rpi4m3@192.168.5.104" "rpi4m4@192.168.5.108" "rpi4m5@192.168.5.107" "rpi4compute@192.168.5.109" "nuc01@192.168.5.68")
+remote_nodes=("ssh rpi4m1@192.168.5.105" "ssh rpi4m2@192.168.5.106" "ssh rpi4m3@192.168.5.104" "ssh rpi4m4@192.168.5.108" "ssh rpi4m5@192.168.5.107" "ssh rpi4compute@192.168.5.109" "ssh nuc01@192.168.5.68")
 
-for node_name in ${remote_nodes[@]}
+for node_ssh in ${remote_nodes[@]}
 do
-    cmd_git="ssh ${node_name} 'cd \$HOME/OTA-C; git stash; git pull'"
-    echo $cmd_git
-    eval $cmd_git
-    echo "Git update on $node_name complete!"
-    eval "ssh ${node_name} 'cd \$HOME/OTA-C/cpp/build/; cmake ../; make -j4'"
+    cmd="${node_ssh} screen -dmS git_pull_n_make bash -c 'cd \$HOME/OTA-C && git stash && git pull && cd \$HOME/OTA-C/cpp/build/ && cmake ../ && make -j4 && echo \"Success\"'"
+    echo $cmd
+    eval $cmd
+    # echo "Git update on $node_name complete!"
+    # eval "ssh ${node_name} 'cd \$HOME/OTA-C/cpp/build/; cmake ../; make -j4'"
 done
