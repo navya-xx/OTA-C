9a0661f8f727c7c500f0a196e806f42e4b75b66d
navya-xx <navneet.gr8@gmail.com>
2024-05-20 16:13:13 +0200

    asdf

--- shell/node_git_update.sh
+++ shell/node_git_update.sh
@@ -2,6 +2,12 @@
 
 remote_nodes=("rpi4m1@192.168.5.105" "rpi4m2@192.168.5.106" "rpi4m3@192.168.5.104" "rpi4m4@192.168.5.108" "rpi4m5@192.168.5.107" "rpi4compute@192.168.5.109" "nuc01@192.168.5.68")
 
+cmd="screen -dmS git_pull_n_make bash -c 'cd \$HOME/OTA-C && cd \$HOME/OTA-C/cpp/build/ && cmake ../ && make -j4 && echo \"Success\"'"
+
+echo $cmd
+
+screen -dmS git_pull_n_make bash -c "cd \$HOME/OTA-C && cd \$HOME/OTA-C/cpp/build/ && cmake ../ && make -j4 && echo \"Success\""
+
 for node_name in ${remote_nodes[@]}
 do
     cmd="ssh ${node_name} screen -dmS git_pull_n_make bash -c 'cd \$HOME/OTA-C && git stash && git pull && cd \$HOME/OTA-C/cpp/build/ && cmake ../ && make -j4 && echo \"Success\"'"
