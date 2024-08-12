c51b6c2885cc6cdde17a4f79281ac996f47f1a4c
navya-xx <navneet.gr8@gmail.com>
2024-08-12 15:21:28 +0200

    asdf

--- ProjectRoot/main/centralized_arch/leaf.cpp
+++ ProjectRoot/main/centralized_arch/leaf.cpp
@@ -61,7 +61,7 @@ void producer_thread(USRP_class &usrp_obj, PeakDetectionClass &peakDet_obj, Cycl
         if (stop_signal_called)
             break;
 
-        LOG_INFO_FMT("Estimated Clock Drift = %.8f samples/sec.", csd_obj.estimated_sampling_rate_offset);
+        // LOG_INFO_FMT("Estimated Clock Drift = %.8f samples/sec.", csd_obj.estimated_sampling_rate_offset);
         // usrp_obj.adjust_for_freq_offset(csd_obj.estimated_sampling_rate_offset);
         // LOG_INFO_FMT("Corrected Clock Drift -> New sampling rate = %1% samples/sec.", usrp_obj.rx_rate);
 
