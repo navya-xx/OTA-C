e5ce3d1f55b03ba180b0d3556fb10d2d310c4c82
navya-xx <navneet.gr8@gmail.com>
2024-09-24 14:33:51 +0200

    asdf

--- ProjectRoot/include/lib_waveform/waveforms.hpp
+++ ProjectRoot/include/lib_waveform/waveforms.hpp
@@ -22,7 +22,7 @@ public:
     void initialize(WAVEFORM_TYPE wf_type, size_t wf_len, size_t wf_reps = 1, size_t wf_gap = 0, size_t init_wf_pad = 0, size_t zfc_q = 1, float scale = 1.0, size_t rand_seed = 0);
 
     size_t wf_len, wf_reps, wf_gap, wf_pad, zfc_q, rand_seed;
-    float scale = 1.0, pad_scale = 0.0005 - ;
+    float scale = 1.0, pad_scale = 0.0005;
 
     std::vector<std::complex<float>> generate_waveform();
 
